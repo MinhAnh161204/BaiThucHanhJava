200e92dab8079b845c8df001dd9f4efdbcfd0e71
MinhAnh161204 <nguyenleminhanh.sedu@gmail.com>
2024-03-08 15:43:53 +0700

    Bai2

--- Lab2/src/Bai2.java
+++ Lab2/src/Bai2.java
@@ -7,6 +7,6 @@ public class Bai2 {
         int n = sc.nextInt();
         if(n%2==0) System.out.println("n la so chan");
         else System.out.println("n la so le");
-        
+        sc.close();
     }
 }
