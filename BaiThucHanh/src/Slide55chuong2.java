a2a373510648d3bb09a71794148dd4995ed6cebc
MinhAnh161204 <nguyenleminhanh.sedu@gmail.com>
2024-03-08 15:37:28 +0700

    Slide55chuong2

--- BaiThucHanh/src/Slide55chuong2.java
+++ BaiThucHanh/src/Slide55chuong2.java
@@ -2,7 +2,7 @@ public class Slide55chuong2 {
     public static void main(String[] args) {
         for(int i=0;i<=20;i++) {
             if(i % 5 == 0) {
-                System.out.print(i + " ");
+                System.out.print(i + "  ");
             }
         }
     }
