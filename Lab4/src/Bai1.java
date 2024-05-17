52e83638f6fb2310c19155d6055034ac48ebe6ac
MinhAnh161204 <nguyenleminhanh.sedu@gmail.com>
2024-05-17 08:46:19 +0700

    Bai1lab4

--- Lab4/src/Bai1.java
+++ Lab4/src/Bai1.java
@@ -1,32 +1,65 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arrayListInt = new ArrayList<>(10);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        
+        System.out.print("Nhap so phan tu : ");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            System.out.print("Nhap gia tri phan tu thu " + (i + 1) + " : ");
+            int value = sc.nextInt();
+            arrayList.add(value);
+        }
 
-        System.out.println("Nhap so phan tu: ");
-        int number;
-        while (true) {
-            number = sc.nextInt();
-            if (number == -1) {
-                break;
+        System.out.println("Gia tri lon nhat cua mang la : " + maxValues(arrayList));
+        removeValues(arrayList);
+        System.out.println("Danh sach phan tu sau khi xoa : ");
+        showList(arrayList);
+        System.out.println("Cac phan tu sau khi sap xep : ");
+        showList(sortList(arrayList));
+    }
+    
+    private static void showList(ArrayList<Integer> arrayList) {
+        for(int x : arrayList) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+    private static int maxValues(ArrayList<Integer> arrayList) {
+        int max = arrayList.get(0);
+        for(int x : arrayList) {
+            if(x > max) {
+                max = x;
             }
-            arrayListInt.add(number);
         }
-        if (!arrayListInt.isEmpty()) {
-            int max = Collections.max(arrayListInt);
-            System.out.println("So nguyen lon nhat la: " + max);
-        } else {
-            System.out.println("Day so rong.");
-        }
-        System.out.print("Nhap so nguyen can xoa: ");
-        int numberToRemove = sc.nextInt();
-        arrayListInt.removeIf(n -> n == numberToRemove);
+        return max;
+    }
 
-        Collections.sort(arrayListInt);
-        System.out.println("Day so sau khi sap xep la: " + arrayListInt);
+    private static void removeValues(ArrayList<Integer> arrayList) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap phan tu can xoa : ");
+        int values = sc.nextInt();
+
+        if(arrayList.contains(values)) {
+            for(int i = 0; i < arrayList.size(); i++) {
+                if(arrayList.get(i) == values) {
+                    arrayList.remove(i);
+                }
+            }
+            System.out.println("Xoa du lieu thanh cong !");
+            System.out.println();
+        } else {
+            System.out.println("khong ton tai gia tri do ...");
+        }
+    }
+
+    private static ArrayList<Integer> sortList(ArrayList<Integer> arrayList) {
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        arrayList.sort((o1, o2) -> (o1 - o2));
+        newArrayList = arrayList;
+        return newArrayList;
     }
 }
